1406a790cb06fb90c1481379aceb799d3b1d1785
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2020-11-09 20:47:05 +0300

    Import electron auto updater and setup logger

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import electron from 'electron';
 
 const { app, BrowserWindow } = electron;
 
+const { autoUpdater } = require('electron-updater');
+const logger = require('electron-log');
+logger.transports.file.level = 'info';
+logger.transports.file.file = __dirname + '..' + 'log.log';
+
 let mainWindow;
 
 app.on('ready', () => {
