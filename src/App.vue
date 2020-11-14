57bf6eab7bc6924e97297d20c4ea70effcbc4d9f
fabl <34402607+fabled228@users.noreply.github.com>
2020-11-14 19:02:23 +0300

    electron+vue interation added & deleted babel

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,8 @@
   <h1>devops-toolbox-app</h1>
 </template>
 
-<script>
+<script lang="ts">
 export default {
   name: 'App',
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import electron from 'electron';
-
-const { app, BrowserWindow } = electron;
-
-let mainWindow;
-
-app.on('ready', () => {
-  mainWindow = new BrowserWindow({
-
-  });
-  mainWindow.loadURL(`${process.cwd()}/public/index.html`);
-});
+// import electron from 'electron';
+//
+// const { app, BrowserWindow } = electron;
+//
+// let mainWindow;
+//
+// app.on('ready', () => {
+//   mainWindow = new BrowserWindow({
+//
+//   });
+//   mainWindow.loadURL(`${process.cwd()}/public/index.html`);
+// });
