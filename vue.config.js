8639c0ee97a35738790a88bba60bfe94078b4cd4
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2020-11-20 11:42:32 +0300

    Fix vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   pluginOptions: {
     electronBuilder: {
-      mainProcessFile: 'src/index.ts',
+      disableMainProcessTypescript: false,
+      mainProcessTypeChecking: false,
+      builderOptions: {
+        files: [
+          'src/**',
+          'public/**',
+          'package.json',
+          'node_modules',
+        ],
+        win: {
+          target: 'nsis',
+        },
+      },
     },
   },
 };
