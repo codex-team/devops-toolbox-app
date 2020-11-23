ff4323031b83e29b40cda7a01cdd97620b6ce932
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2020-11-23 21:28:59 +0300

    Add buttons

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <template>
-  <h1>devops-toolbox-app</h1>
+  <div>
+    <h1>devops-toolbox-app</h1>
+    <button id="getWorkspaces" @click="getWorkspaces">Get Workspaces</button>
+    <button id="authorization" @click="logIn">Log In</button>
+    <div id="workspaces"></div>
+  </div>
 </template>
 
 <script lang="ts">
-export default {
+import Transport from '@/utils/protocol/transport';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
   name: 'App',
-};
+  data() {
+    return {
+      transport: new Transport(),
+    };
+  },
+  created() {
+  },
+  methods: {
+    getWorkspaces() {
+      this.transport.send('getWorkspaces', {
+        authToken: 1,
+      });
+    },
+    logIn() {
+      this.transport.send('authorization', {
+        authToken: 1,
+      });
+    },
+  },
+});
+
 </script>
