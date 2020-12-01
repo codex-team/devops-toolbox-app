e3d31d04df4e1b073747e9c6132218408e917461
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2020-12-01 22:46:52 +0300

    Added function for notifications

---
+++ actions/notification.ts
@@ -0,0 +1,17 @@
+import { Notification } from 'electron';
+
+/**
+ * Function for showing notification for
+ *
+ * @param message - Message which will be displayed in the notification
+ */
+function showNotification(message: string): void {
+  const notification = {
+    title: 'CodeX DevOps Toolbox',
+    body: `${message}`,
+  };
+
+  new Notification(notification).show();
+}
+
+export default showNotification;
