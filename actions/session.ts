f37e8127c49e75c40f904160911c63e2b843afea
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2020-12-02 17:01:35 +0300

    Refactor code

--- actions/session.ts
+++ actions/session.ts
@@ -6,15 +6,16 @@ import * as childProcess from 'child_process';
  * @param command - command with server address
  */
 const session = function (command: string): void {
-  if (process.platform == 'win32') {
-    childProcess.exec(`start cmd.exe /K ${command}`);
-  }
-  if (process.platform == 'darwin') {
-    childProcess.exec(`osascript -e 'tell application "Terminal" to (do script "${command}") activate'`);
-  }
-  if (process.platform == 'linux') {
-    const terminal = 'gnome-terminal';
-    childProcess.spawn(terminal);
+  switch (process.platform) {
+    case 'win32':
+      childProcess.exec(`start cmd.exe /K ${command}`);
+      break;
+    case 'darwin':
+      childProcess.exec(`osascript -e 'tell application "Terminal" to (do script "${command}") activate'`);
+      break;
+    case 'linux':
+      childProcess.spawn('gnome-terminal');
+      break;
   }
 };
 
