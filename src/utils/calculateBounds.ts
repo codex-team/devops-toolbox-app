4351ade22492f64579c1a3d3bcbd1fb5ac0c4cd9
fabl <34402607+fabled228@users.noreply.github.com>
2020-12-20 18:09:56 +0300

    fixed jsDoc

--- src/utils/calculateBounds.ts
+++ src/utils/calculateBounds.ts
@@ -1,11 +1,12 @@
-
 /**
+ * Calculating Bounds for browserWindow
+ *
  * @param x - x of mouse click
  * @param y - x of mouse click
  * @param height - height of window
  * @param width - width of window
  */
-export default function calculateBounds(x: number, y: number, height: number, width: number): { x: number, y: number } {
+export default function calculateBounds(x: number, y: number, height: number, width: number): { x: number; y: number } {
   const half = 0.5;
 
   return {
