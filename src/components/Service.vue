70683029edd79224b473c2e055e25b5c92e436b0
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2021-01-15 21:00:38 +0300

    Create Service.vue

---
+++ src/components/Service.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Project
+      v-for="project in projects"
+      :key="project.name"
+      :name="project.serverName"
+      :status="project.status"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Project from '@/components/Project.vue';
+
+export default defineComponent({
+  name: 'Service',
+  components: {
+    Project,
+  },
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+    projects: {
+      type: Array,
+      required: true,
+    },
+  },
+});
+</script>
