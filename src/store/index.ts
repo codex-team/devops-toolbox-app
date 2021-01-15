82782188b938b01cdf18097af4dc05e9a2240d01
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2021-01-15 20:59:13 +0300

    Created store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,18 @@
 import { createStore } from 'vuex';
+import { Workspace } from '@/types';
 
 export default createStore({
   state: {
-    isAuth: false,
+    workspaces: new Array<Workspace>(),
   },
   mutations: {
-    authSuccess(state) {
-      state.isAuth = true;
+    workspacesUpdate(state, workspaces: Workspace[]) {
+      state.workspaces = workspaces;
     },
-  },
-  getters: {
-    isAuth: state => state.isAuth,
+    workspaceUpdate(state, workspace: Workspace) {
+      const index = state.workspaces.findIndex(w => workspace.name === w.name);
+
+      state.workspaces[index] = workspace;
+    },
   },
 });
--- src/store/vuex.d.ts
+++ src/store/vuex.d.ts
@@ -1,9 +1,11 @@
 import { ComponentCustomProperties } from 'vue';
 import { Store } from 'vuex';
+import { Workspace } from '@/types';
 
 declare module '@vue/runtime-core' {
   interface State {
     isAuth: boolean,
+    workspaces: Workspace[],
   }
 
   interface ComponentCustomProperties {
