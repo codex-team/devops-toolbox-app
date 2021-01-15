24dcd0ff8d353a117a053e28e1e78417e4f42103
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2021-01-15 20:59:24 +0300

    Created project type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,11 @@
 import Workspace from './workspace';
 import Server from './server';
 import Service from './service';
+import Project from './project';
 
 export {
   Workspace,
   Server,
-  Service
+  Service,
+  Project
 };
---
+++ src/types/project.ts
@@ -0,0 +1,16 @@
+/**
+ * Project hosted on the server
+ */
+export default interface Project {
+  /**
+     Project name
+   */
+  name: string,
+  /**
+   * Project status
+   *
+   * true - is available
+   * false - is not avaliable
+   */
+  status: boolean,
+}
