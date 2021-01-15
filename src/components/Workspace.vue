938bd4036fa6594d98f84f1b3cba41061b3e170e
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2021-01-15 21:00:33 +0300

    Create Workspace.vue

---
+++ src/components/Workspace.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <WorkspaceHeader
+      :name="name"
+      :image="image"
+    />
+    <Server
+      v-for="(server, index) in servers"
+      :key="server.name"
+      :name="server.name"
+      :services="server.services"
+      :hotkey="index+1"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import WorkspaceHeader from '@/components/WorkspaceHeader.vue';
+import Server from '@/components/Server.vue';
+
+export default defineComponent({
+  name: 'Workspace',
+  components: {
+    WorkspaceHeader,
+    Server,
+  },
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    servers: {
+      type: Object,
+      required: true,
+    },
+  },
+});
+</script>
