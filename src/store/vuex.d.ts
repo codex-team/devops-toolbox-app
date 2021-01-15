bb4eb11a560afa29c6e29be944345607d4af6c47
Oybek Muslimov <64841603+kebyo@users.noreply.github.com>
2021-01-15 21:05:04 +0300

    Removed isAuth

--- src/store/vuex.d.ts
+++ src/store/vuex.d.ts
@@ -1,10 +1,8 @@
-import { ComponentCustomProperties } from 'vue';
 import { Store } from 'vuex';
 import { Workspace } from '@/types';
 
 declare module '@vue/runtime-core' {
   interface State {
-    isAuth: boolean,
     workspaces: Workspace[],
   }
 
