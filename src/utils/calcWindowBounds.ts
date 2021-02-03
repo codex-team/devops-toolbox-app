69934d03ae55be161eab95d5a9315024d6caf220
fabl <34402607+fabled228@users.noreply.github.com>
2021-02-03 21:04:26 +0300

    Update calcWindowBounds.ts
    added new variables for dock width&height

--- src/utils/calcWindowBounds.ts
+++ src/utils/calcWindowBounds.ts
@@ -13,22 +13,23 @@ export default function calcWindowBounds(windowPosition: WindowPosition, win: Br
   const displayWidth = display.bounds.width;
   let xPosition;
   let yPosition;
+  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+  const dockHeight = Math.round(height * 0.1);
+  // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+  const dockWidth = width * 0.5;
 
   if (windowPosition === 'trayBottomLeft') {
-    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-    xPosition = Math.round(x + width * 0.5);
+    xPosition = Math.round(x + dockWidth);
   } else if (windowPosition === 'trayBottomRight') {
     // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-    xPosition = Math.round(displayWidth - width * 1.5);
+    xPosition = Math.round(displayWidth - width + dockWidth);
   } else {
-    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-    xPosition = Math.round(x - width * 0.5);
+    xPosition = Math.round(x - dockWidth);
   }
   if (windowPosition === 'trayCenter' && process.platform === 'darwin') {
     yPosition = y;
   } else if (windowPosition === 'trayCenter') {
-    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-    yPosition = y + Math.round(height * 0.1);
+    yPosition = y + dockHeight;
   } else {
     yPosition = y - height;
   }
