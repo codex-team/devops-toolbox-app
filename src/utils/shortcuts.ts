da9158c78e03e797f84865018b4be7b0c25b92b4
Kanstantsin Matyshau <kostya.matycheff@gmail.com>
2021-03-18 20:47:23 +0300

    fixed

--- src/utils/shortcuts.ts
+++ src/utils/shortcuts.ts
@@ -11,23 +11,26 @@ export function enableShortcuts(workspaces: Workspace[]): void {
   const SHORTCUTS_MAX = 9;
   let SHORTCUTS_COUNT = 0;
 
-  for (const workspace of workspaces) {
-    for (const server of workspace.servers) {
-      if (SHORTCUTS_COUNT < SHORTCUTS_MAX) {
-        SHORTCUTS_COUNT++;
-        globalShortcut.register(`CommandOrControl+${SHORTCUTS_COUNT}`, () => {
-          if(server.sshConnectionInfo) {
-            let command = `ssh ${server.sshConnectionInfo.username}@${server.sshConnectionInfo.ip}`;
+  if (workspaces) {
+    for (const workspace of workspaces) {
+      for (const server of workspace.servers) {
+        console.dir(server);
+        if (SHORTCUTS_COUNT < SHORTCUTS_MAX) {
+          SHORTCUTS_COUNT++;
+          globalShortcut.register(`CommandOrControl+${SHORTCUTS_COUNT}`, () => {
+            if (server.sshConnectionInfo) {
+              let command = `ssh ${server.sshConnectionInfo.username}@${server.sshConnectionInfo.ip}`;
 
-            if (server.sshConnectionInfo.port) {
-              command += ` -p ${server.sshConnectionInfo.port}`;
+              if (server.sshConnectionInfo.port) {
+                command += ` -p ${server.sshConnectionInfo.port}`;
+              }
+
+              openSession(command);
             }
-
-            openSession(command);
-          }
-        });
-      } else {
-        return;
+          });
+        } else {
+          return;
+        }
       }
     }
   }
